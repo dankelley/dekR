5b7b0b6b33a91ed3761e36263be5ebfc3e54160e
dankelley <kelley.dan@gmail.com>
2023-04-15 08:59:59 -0300

    first commit

---
+++ R/derivative.R
@@ -0,0 +1,42 @@
+#' Numerical derivative
+#'
+#' If the number of points exceeds 2, then `derivative()` computes centred
+#' derivatives for interior points, and first differences at the endpoints.
+#' If it is 2, then `derivative()` computes the first difference and repeats
+#' the value. Otherwise, an error is reported.
+#'
+#' @param x a numerical vector holding the independent coordinate. An error is
+#' reported if `x` holds under 3 points.
+#'
+#' @param y a numerical vector of the same length as `x`, holding the dependent
+#' coordinate, y=y(x).
+#'
+#' @examples
+#'
+#' # Demonstrate with a known derivative
+#' x <- seq(0, 2*pi, pi/16)
+#' y <- sin(x)
+#' dydx <- derivative(x, y)
+#' layout(rbind(1,2), heights=c(0.6, 0.4))
+#' par(mar=c(3,3,1,1), mgp=c(2,0.7,0))
+#' plot(x, cos(x))
+#' points(x, dydx, col=2, pch="+")
+#' plot(x, dydx-cos(x))
+#'
+#' @return derivative dy/dx
+#' @author Dan Kelley
+#' @export
+derivative <- function(x, y)
+{
+    n <- length(x)
+    if (length(y) != n)
+        stop("x and y must have equal length, but the lengths are ", n, " and ", length(y))
+    if (n > 2) {
+        c((y[2L]-y[1L]) / (x[2L]-x[1L]),
+            diff(y, lag=2L) / diff(x, lag=2L),
+            (y[n]-y[n-1L]) / (x[n]-x[n-1L]))
+    } else {
+        rep((y[2]-y[1]) / (x[2]-x[1]), 2L)
+    }
+}
+
